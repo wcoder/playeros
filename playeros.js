98dd24504955fde24468dacf5df90ad5bb205397
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-03-04 01:44:02 +0300

    [*] Changed player controls and styles

--- playeros.js
+++ playeros.js
@@ -1,109 +1,163 @@
 (function (w) {
     'use strict';
 
+    var helper = {
+        durationToStringConverter: function (v) {
+            var s = parseInt(v % 60);
+            var m = parseInt((v / 60) % 60);
+            return m + '.' + s + 'min';
+        },
+        createElement: function (tagName, className, parentElement) {
+            var e = document.createElement(tagName);
+            e.className = className;
+            if (typeof parentElement != 'undefined') {
+                parentElement.appendChild(e);
+            }
+            return e;
+        }
+    };
+
+    function ControlPanel(playCallback, prevCallback, nextCallback)
+    {
+        /* private */
+
+        var self = this,
+            panel = helper.createElement('div', 'playeros-panel'),
+            play = helper.createElement('button', 'playeros-play', panel),
+            prev = helper.createElement('button', 'playeros-prev', panel),
+            next = helper.createElement('button', 'playeros-next', panel),
+            progress = helper.createElement('div', 'playeros-progress', panel),
+            progressProcess = helper.createElement('div', 'playeros-progress-process', progress),
+            composition = helper.createElement('div', 'playeros-composition', panel),
+            time = helper.createElement('div', 'playeros-time', panel),
+            currentTime = helper.createElement('div', 'playeros-time-current', time),
+            endTime = helper.createElement('div', 'playeros-time-end', time);
+
+        play.addEventListener('click', function (e) {
+            playCallback(self, e);
+        });
+        prev.addEventListener('click', function (e) {
+            prevCallback(self, e);
+        });
+        next.addEventListener('click', function (e) {
+            nextCallback(self, e);
+        });
+
+        /* public */
+
+        this.panel = panel;
+
+        this.play = function () {
+            play.className = 'playeros-play playeros-play-pause';
+        };
+        this.pause = function () {
+            play.className = 'playeros-play';
+        };
+        //this.stop = function () {
+        //    play.className = 'playeros-play playeros-play-stop';
+        //};
+        this.progress = function (percent) {
+            progressProcess.style.width = percent + '%';
+        };
+        this.changeComposition = function (value) {
+            composition.innerText = value;
+        };
+        this.setCurrentTime = function (value) {
+            currentTime.innerText = value;
+        };
+        this.setEndTime = function (value) {
+            endTime.innerText = value;
+        };
+    }
+
+
     /**
      * Class for create custom HTML5 audio player
      * @author Evgeniy Pakalo <evgeniy.pakalo[g]gmail.com>
      * @param element
      * @param sources
+     * @param options
      * @constructor
      */
     w.Playeros = function (element, sources, options) {
 
-        var currentSource = 0;
-        var played = false;
-        var settings = {
-            volume: 0.5
-        };
+        var currentSourceIndex = 0,
+            isPlayed = false,
+            settings = {
+                volume: 0.5
+            },
+            panel = new ControlPanel(play, prev, next),
+            player = createPlayer();
 
-        var play = element.querySelector('.playeros-play');
-        var volume = element.querySelector('.playeros-range');
-        var progress = element.querySelector('.playeros-progress');
-        var currentTime = element.querySelector('.playeros-currenttime');
-        var endTime = element.querySelector('.playeros-endtime');
-        var composition = element.querySelector('.playeros-composition');
-        var back = element.querySelector('.playeros-back');
-        var next = element.querySelector('.playeros-next');
-        var player = element.querySelector('.playeros-player');
-
-        var playCurrentSource = function () {
-            var source = sources[currentSource];
-            composition.innerText = source.name;
-            player.src = source.file;
-            player.play();
-        };
-
-        var nextSource = function () {
-            if (currentSource < sources.length - 1) {
-                ++currentSource;
-            } else {
-                currentSource = 0;
-            }
-        };
-
-        var durationToStringConverter = function (v) {
-            var s = parseInt(v % 60);
-            var m = parseInt((v / 60) % 60);
-            return m + '.' + s + 'sec';
-        };
-
-
-        var init = function () {
+        (function init() {
             _.extend(settings, options);
 
+            changeCurrentSource();
 
-            volume.value = settings.volume * 10;
-        };
+            element.appendChild(panel.panel);
+        }());
 
-        play.onclick = function () {
-            if (!played) {
-                playCurrentSource();
-                play.innerText = '||';
-            } else {
+
+        function createPlayer() {
+            var p = helper.createElement('audio');
+            p.preload = 'metadata';
+            p.type = 'audio/mpe';
+            p.volume = settings.volume;
+            p.onplaying = playerStart;
+            p.ontimeupdate = playerProgress;
+            p.onended = next;
+            return p;
+        }
+
+        function play(panel) {
+            if (isPlayed) {
                 player.pause();
-                play.innerText = '>';
-            }
-            played = !played;
-        };
-
-        volume.onchange = function () {
-            player.volume = parseFloat(volume.value / 10);
-        };
-
-        back.onclick = function () {
-            if (currentSource == 0) {
-                currentSource = sources.length - 1;
+                panel.pause();
             } else {
-                currentSource--;
+                player.play();
+                panel.play();
+            }
+            isPlayed = !isPlayed;
+        }
+
+        function prev() {
+            if (currentSourceIndex == 0) {
+                currentSourceIndex = sources.length - 1;
+            } else {
+                currentSourceIndex--;
             }
             playCurrentSource();
-        };
+        }
 
-        next.onclick = function () {
-            nextSource();
-            playCurrentSource();
-        };
-
-        player.onplaying = function () {
-            endTime.innerText = durationToStringConverter(player.duration);
-        };
-
-        player.ontimeupdate = function () {
-            try {
-                progress.max = Math.floor(player.duration);
-                progress.value = Math.floor(player.currentTime);
-                currentTime.innerText = durationToStringConverter(player.currentTime);
-            } catch (e) {
-                console.error(e);
+        function next() {
+            if (currentSourceIndex < sources.length - 1) {
+                ++currentSourceIndex;
+            } else {
+                currentSourceIndex = 0;
             }
-        };
-
-        player.onended = function () {
-            nextSource();
             playCurrentSource();
-        };
+        }
 
-        init();
+        function playCurrentSource() {
+            changeCurrentSource();
+            player.play();
+            panel.play();
+        }
+
+        function changeCurrentSource() {
+            var source = sources[currentSourceIndex];
+            panel.changeComposition(source.name);
+            player.src = source.file;
+        }
+
+        function playerProgress() {
+            panel.progress((player.currentTime * 100) / player.duration);
+            panel.setCurrentTime(helper.durationToStringConverter(player.currentTime));
+        }
+
+        function playerStart() {
+            panel.setEndTime(helper.durationToStringConverter(player.duration));
+        }
     };
 
 }(window));
